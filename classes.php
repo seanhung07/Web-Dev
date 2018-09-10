f9ee1d341e674fb77a813c665f13f4ca9a6ecd67
danielchen09 <danielchen@pacificamerican.org>
2018-09-10 09:26:38 +0800

    Add files via upload

---
+++ classes.php
@@ -0,0 +1,16 @@
+<?php
+  ini_set('display_errors', 1);
+  ini_set('displat_startup_errors', 1);
+  error_reporting(E_ALL);
+  include("../classes.php");
+  session_start();
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+
+  <body>
+    <h1></h1>
+  </body>
+</html>
